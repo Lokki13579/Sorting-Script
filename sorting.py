3d920b4d5b44bd2b3f012968a8cde2b96e8ecacc
Lokki13579 <l42@lokki13579.ru>
2026-02-04 01:17:17 +0300

    Файл форматов перенесен в папку конфигов Добавлен импорт настроек из любого файла флагом -i и путем к импортируемому файлу

--- sorting.py
+++ sorting.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 import json
+import os
 import re
 import subprocess
 import sys
@@ -20,23 +21,28 @@ class Sorting:
                     )
                     break
                 case ["-a", arg]:
-                    new_category, new_format = arg.split(":")
+                    self.__add_new_format(*arg.split(":"))
+                    self.save_formats()
+                case ["-r", arg]:
+                    old_category, new_category = arg.split(":")
+                    if old_category not in self.formats:
+                        self.formats[old_category] = []
                     if new_category not in self.formats:
                         self.formats[new_category] = []
                     for cat, formats in self.formats.items():
-                        if new_format in formats:
-                            formats.remove(new_format)
-                    self.formats[new_category].append(new_format)
-                    self.save_formats()
-                case ["-d", arg]:
-                    for key, vals in self.formats.items():
-                        if arg in vals:
-                            vals.remove(arg)
+                        if old_category in formats:
+                            formats.remove(old_category)
+                            self.formats[new_category].append(old_category)
                             self.save_formats()
-                            print("removed", arg)
+                            print("moved", old_category, "to", new_category)
                             break
                     else:
-                        print("Unknown format")
+                        print("Unknown category")
+                case ["-d", arg]:
+                    self.__delete_format(arg)
+                    self.save_formats()
+                case ["-i", arg]:
+                    self.__import_settings(arg)
                 case ["-n"]:
                     break
                 case [format]:
@@ -49,6 +55,29 @@ class Sorting:
                     print("Unknown argument")
                     break
             args = args[2:]
+
+    def __add_new_format(self, category, format):
+        if category not in self.formats:
+            self.formats[category] = []
+        self.__delete_format(format)
+        self.formats[category].append(format)
+
+    def __delete_format(self, format):
+        for key, vals in self.formats.items():
+            if format in vals:
+                vals.remove(format)
+                print("removed", format)
+                break
+        else:
+            print("Unknown format")
+
+    def __import_settings(self, file_from):
+        try:
+            with open(file_from) as f:
+                self.formats = json.load(f)
+                self.save_formats()
+        except FileNotFoundError:
+            self.formats = {}
 
     def sorting(self, thisformat=None):
         if thisformat == None:
@@ -74,17 +103,25 @@ class Sorting:
                         stdout=subprocess.PIPE,
                     )
 
+    def get_formats_path(self):
+        return os.path.join(os.path.expanduser("~/.config/sorting"), "formats.json")
+
     def import_formats(self):
-        with open("formats.json") as file:
+        file_path = self.get_formats_path()
+        if not os.path.exists(file_path):
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            with open(file_path, "w") as file:
+                json.dump({}, file)
+        with open(file_path) as file:
             self.formats = json.load(file)
         print("imported", self.formats)
 
     def save_formats(self):
         print(self.formats, "- saving")
-        with open("formats.json", "w", encoding="utf-8") as f:
+        file_path = self.get_formats_path()
+        with open(file_path, "w", encoding="utf-8") as f:
             json.dump(self.formats, f, indent=4)
 
 
-print(sys.argv[1:])
 if __name__ == "__main__":
     Sorting().process(sys.argv[1:])
